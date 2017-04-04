26cacc494f83340ffd66808187144cacb8273580
jeanmarielepioufle <jm@nexmodeling.com>
2017-04-04 11:14:45 +0200

    made public

---
+++ R/catchment_norway.R
@@ -0,0 +1,12 @@
+#' Norwegian catchments
+#'
+#' Catchment shapefile convert into R format.
+#' The original dataset is available at http://nedlasting.nve.no/gis/
+#'
+#' @docType data
+#' @name catchment_norway
+#' @format A rda file
+#' @keywords datasets
+#' @references the Norwegian Water Resources and Energy Directorate (NVE)
+#' @source \href{http://www.nve.no}{NVE}
+NULL
